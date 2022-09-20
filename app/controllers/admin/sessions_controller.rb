f39315584560144fd8f084add4293ad7c69b97d5
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-09-20 13:40:38 +0000

    admin/login画面の作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,7 +5,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   # def new
-  #   super
+  #  super
   # end
 
   # POST /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,37 @@ Rails.application.routes.draw do
   }
   
   
+  namespace :admin do
+    devise_scope :admin do
+      get '/sign_in' => 'sessions#new'
+      post '/sign_in' => 'sessions#create'
+      delete '/sign_out' => 'sessions#destroy'
+    end  
+    resources :items
+    resources :genres
+    resources :customers
+    resources :orders
+    resources :order_details
+  end
+  
+  
+  
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  
+  namespace :public do
+    resources :items
+    resources :customers
+    resources :cart_items
+    resources :orders
+    resources :addresses
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
