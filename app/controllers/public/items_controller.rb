d79d8ac41bf549dfaf5e655c52148709f0cf2c41
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-04 13:15:08 +0000

    商品一覧画面（会員）

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,3 +1,10 @@
 class Public::ItemsController < ApplicationController
+  def index
+    @customer = current_customer
+        
+    #ページネーション
+    @pages = @items.all.page(params[:page])
+  end
+
   
 end
