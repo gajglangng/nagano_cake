2e380f6219e01c38d0bee79494ed69eef34fc978
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-05 10:55:51 +0000

    配送先登録画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,19 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer!
+
+  def index
+    @address = Address.new
+    @customer = Customer.find(current_customer.id)
+    @addresses = @customer.shipping_addresses
+  end
+
+  
+
+
+private
+
+def address_params
+  params.require(:address).permit(:name, :address, :postcode)
+end
+
 end
