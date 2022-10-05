4c34a3e659ce53c888bdbc48c688529deb3e9b89
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-05 12:29:26 +0000

    会員情報編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,7 +9,18 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def edit
+    @customer = Customer.find(params[:id])
+  end
   
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(params[:id])
+    else
+      render 'edit'
+    end
+  end
 
   private
   def customer_params
