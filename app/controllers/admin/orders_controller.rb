5d0bdb0f9300520252c2a46979d0cdbf3f6e7c98
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-05 12:55:45 +0000

    注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def show #注文履歴詳細
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
+    @total_fee = (@order.billing_amount - @order.shipping_fee).to_s(:delimited)
+  end
+   
+  def order_params
+    params.require(:order)
+  end 
 end
