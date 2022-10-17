34ee2504b6d0ab4049e4cfeec97247d94f160bc3
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-17 12:43:20 +0000

    migrate追記

--- db/migrate/20221004125020_create_items.rb
+++ db/migrate/20221004125020_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
+      t.integer :genre_id
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.string :image_id
+      t.boolean :is_sale
       t.timestamps
     end
   end
--- db/migrate/20221015080239_create_orders.rb
+++ db/migrate/20221015080239_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
+      t.integer :customer_id
+      t.integer :billing_amount
+      t.integer :payment_method, default: 0
+      t.string :postcode
+      t.string :address
+      t.string :name
+      t.integer :status, default: 0
+      t.timestamps
       t.timestamps
     end
   end
