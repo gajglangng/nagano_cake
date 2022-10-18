fe75c3d1de7a4117e07362e0b7202066785de243
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-18 12:36:12 +0000

    テーブル名

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
   def top
     @customer = current_customer
     # 新着商品
-    @new_items = Item.find(OrderItem.joins(:item).group('item_id').order('count(item_id) desc').limit(4).pluck(:item_id))
+    @new_items = Item.find(Orderdetail.joins(:item).group('item_id').order('count(item_id) desc').limit(4).pluck(:item_id))
   end
   
 end 
--- db/migrate/20221017135336_create_order_details.rb
+++ db/migrate/20221017135336_create_order_details.rb
@@ -1,6 +1,6 @@
-class CreateOrderItems < ActiveRecord::Migration[6.1]
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
-    create_table :order_items do |t|
+    create_table :order_details do |t|
       t.integer :order_id
       t.integer :item_id
       t.integer :price_including_tax
