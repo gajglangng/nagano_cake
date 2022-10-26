60cdc00466634c11c8357bfab670ddfd4f5080bf
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-10-26 14:03:11 +0000

    adminLogin

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,16 +1,19 @@
 class Admin::HomesController < ApplicationController
  before_action :authenticate_admin!
 
-  def top
-   case params[:order_sort]
-   when "0"
-    @orders = Order.where(created_at: Date.today.in_time_zone.all_day).page(params[:page]).per(10)
-   when "1"
-    @customer = Customer.find(params[:customer_id])
-    @orders = @customer.orders.page(params[:page]).per(10)
-   else
-    @orders = Order.all.page(params[:page]).per(10)
-   end
+ def top
+  case params[:order_sort]
+  when "0"
+   @orders = Order.where(created_at: Date.today.in_time_zone.all_day).page(params[:page]).per(10)
+  when "1"
+   @customer = Customer.find(params[:customer_id])
+   @orders = @customer.orders.page(params[:page]).per(10)
+  else
+   @orders = Order.all.page(params[:page]).per(10)
   end
+ end
 
+
+
+ 
 end
