2658616c848bb6f91df245f3d4cce43f0b090780
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-11-07 10:53:10 +0000

    商品一覧

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,20 +1,24 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.find(OrderDetail.joins(:item).group('item_id').order('count(item_id) desc').limit(8).pluck(:item_id))
+    
+    #@items = Item.find(OrderDetail.joins(:item).group('item_id').order('count(item_id) desc').limit(8).pluck(:item_id))
     @customer = current_customer
-    #@items = Item.all
+    @items = Item.all
     
     #@item = Item.find(params[:id])
     #ジャンル検索
     @genres = Genre.where(is_valid: true)
-     if @genre = Genre.find_by(name: params[:name])
-        @items = @genre.items.where(is_active: true).page(params[:page]).reverse_order
-     else
-	      @items = Item.where(is_active: true).where(genre_id: @genres).page(params[:page]).reverse_order
-     end
+     #if @genre = Genre.find_by(name: params[:name])
+      #  @items = @genre.items.where(is_active: true).page(params[:page]).reverse_order
+     #else
+	    #  @items = Item.where(is_active: true).where(genre_id: @genres).page(params[:page]).reverse_order
+     #end
     #ページネーション
     @pages = Item.page(params[:page])
+    
   end
+  
+
 
   def show
     @customer = current_customer
