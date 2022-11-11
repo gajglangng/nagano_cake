c62a5a7f0991c4811d2d3045cb4f56135a8841f5
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-11-11 16:15:06 +0000

    退会確認画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -22,10 +22,10 @@ class Admin::CustomersController < ApplicationController
       render 'edit'
     end
   end
-
+  
   private
-  def customer_params
+   def customer_params
      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
-  end
+   end
 
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,8 +9,17 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(current_customer.id)
   end
   
+  def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render 'edit'
+    end
+  end
+  
   def unsubscribe
-    @customer = current_customer.id
+    @customer = current_customer
   end
 
   def withdraw
@@ -21,14 +30,7 @@ class Public::CustomersController < ApplicationController
     redirect_to root_path
   end
 
-  def update
-    @customer = Customer.find(current_customer.id)
-    if @customer.update(customer_params)
-      redirect_to customers_my_page_path
-    else
-      render 'edit'
-    end
-  end
+ 
 
 
 
