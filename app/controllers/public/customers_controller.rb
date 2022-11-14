49f7c50c5a2df3dbdf898ae35d80373e2c3ddb50
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-11-14 13:20:50 +0000

    public/customer編集遷移

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,7 +13,7 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = Customer.find(current_customer.id)
     if @customer.update(customer_params)
-      redirect_to customers_path
+      redirect_to customers_my_page_path
     else
       render 'edit'
     end
